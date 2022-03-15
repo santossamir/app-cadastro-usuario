9982ef2281a8c3ea845d3e524dda9669165e9060
Samir Santos <santossamir@hotmail.com>
2022-03-15 10:25:27 -0300

    Criando o web service do projeto na pasta backend.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppPrincipal from './AppPrincipal';
+
+const root =  document.getElementById('root');
+ReactDOM.render(
+  <React.StrictMode>
+    <AppPrincipal />
+  </React.StrictMode>,root
+);
