97c143ce2822229d5ea8b6a73b035bc6b064f14a
Samir Santos <santossamir@hotmail.com>
2022-03-16 10:46:19 -0300

    Desenvolvendo o component Nav.

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -4,7 +4,14 @@ import React from 'react';
 export default props =>{
     return(
         <aside className='menu-area'>
-            NAvegação
+            <nav className="menu">
+                <a href="#/">
+                    <i className='fa fa-home'></i> Início
+                </a>
+                <a href="#/users">
+                    <i className='fa fa-users'></i> Usuários
+                </a>
+            </nav>
         </aside>
     )
 }
